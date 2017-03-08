fecaca788102701b48e4c5f3568345e753f3d62c
Manuel Aristaran <jazzido@jazzido.com>
2017-03-07 19:08:46 -0500

    version

--- src/main/java/technology/tabula/tabula_web/App.java
+++ src/main/java/technology/tabula/tabula_web/App.java
@@ -18,6 +18,8 @@ import technology.tabula.tabula_web.workspace.FileWorkspaceDAO;
 
 
 public class App {
+
+	public static final String VERSION = "1.1.0";
 	
 	public static void main(String[] args) throws JsonIOException, JsonSyntaxException, IOException {
 		staticFiles.location("/public");
@@ -29,7 +31,11 @@ public class App {
 	    path("/pdfs/", new PdfsRouteGroup(workspaceDAO));
 	    path("/pdf/", new PdfRouteGroup(workspaceDAO));
 	    path("/queue/", new JobProgressRouteGroup());
-	  
+
+	    get("/version", (req, rsp) -> {
+			rsp.type("application/json");
+			return String.format("{ \"api\": \"%s\" }", VERSION);
+		});
 	}
 }
 
